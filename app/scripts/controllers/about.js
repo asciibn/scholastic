7eda072f7dc143c6dccd8b90f62c12dec227dcfd
Russ Ferguson <rferguson@technologycoach.com>
2016-03-16 12:00:27 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scholasticApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scholasticApp
+ */
+angular.module('scholasticApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
