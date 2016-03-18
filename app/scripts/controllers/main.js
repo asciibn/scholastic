1a170c8415491c6d105238fe5baeb9ae0cf3f7bc
Russ Ferguson <rferguson@technologycoach.com>
2016-03-17 22:12:24 -0400

    fixed a bug where new items were not being deleated

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -21,11 +21,12 @@ angular.module('scholasticApp')
     }
 
     $scope.formItemSubmit = function(){
-        $scope.products.push({'name':$scope.productName, 'description':$scope.productDescription, 'category':'newItem', 'price':$scope.productPrice, 'id':$scope.products.length++});
+        $scope.products.push({'name':$scope.productName, 'description':$scope.productDescription, 'category':'newItem', 'price':$scope.productPrice, 'id':$scope.products.length});
         $scope.searchItemField = $scope.productName;
         $scope.productName = '';
         $scope.productDescription = '';
         $scope.productPrice = '';
+        //console.log($scope.products);
     };
 
     $scope.removeItem = function(e){
@@ -36,7 +37,7 @@ angular.module('scholasticApp')
           $scope.products.splice(i, 1);
         }
       }
-      //console.log($scope.products);
+    //  console.log($scope.products);
     };
 
   loadData();
