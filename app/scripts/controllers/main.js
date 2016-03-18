39ad70343793658a377e1aab563ef5aa4af05cc6
Russ Ferguson <rferguson@technologycoach.com>
2016-03-17 21:33:41 -0400

    can display items from json file and add items from form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,25 @@
  * Controller of the scholasticApp
  */
 angular.module('scholasticApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl',function ($scope, $http) {
+
+
+    function loadData(){
+      $http({method:'GET', url:'scripts/json/products.json'}).then(function getResults(results){
+        $scope.products = results.data;
+        //console.log($scope.products);
+      }, function noResults(response){
+        console.log(response);
+      });
+    }
+
+    $scope.formItemSubmit = function(){
+        $scope.products.push({'name':$scope.productName, 'description':$scope.productDescription, 'category':'newItem', 'price':$scope.productPrice, 'id':$scope.products.length++});
+        $scope.searchItemField = $scope.productName;
+        $scope.productName = '';
+        $scope.productDescription = '';
+        $scope.productPrice = '';
+    };
+
+  loadData();
   });
